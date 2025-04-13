c16f1aaccc06df03e0b26fffaca6009f1730589b
Mamudul_islam <mamuduli153@gmail.com>
2025-04-13 21:23:31 +0600

    product desing

--- src/Attar/seriouspremium.jsx
+++ src/Attar/seriouspremium.jsx
@@ -1,8 +1,162 @@
 import React from 'react'
-
+import ProductCard from '../componentes/Productcard'
 const seriouspremium = () => {
+    const seriouspremium = [
+        {
+            name: 'Royal Sapphire - 6ml',
+            price: 550,
+            discountedPrice: 440,
+            image: 'https://www.believerssign.com.bd/_next/image?url=https%3A%2F%2Fapi.believerssign.com.bd%2Fpublic%2Fproduct%2FAvjgdoY-Bfu5Dir_BAf.jpg&w=1080&q=75',
+            link: '',
+        },
+        {
+            name: 'Red African - 6ml',
+            price: 550,
+            discountedPrice: 440,
+            image: 'https://www.believerssign.com.bd/_next/image?url=https%3A%2F%2Fapi.believerssign.com.bd%2Fpublic%2Fproduct%2FAvjgdoY-Bfu5Dir_BAf.jpg&w=1080&q=75',
+            link: '',
+        }, {
+            name: 'Safa - 6ml',
+            price: 550,
+            discountedPrice: 440,
+            image: 'https://www.believerssign.com.bd/_next/image?url=https%3A%2F%2Fapi.believerssign.com.bd%2Fpublic%2Fproduct%2FAvjgdoY-Bfu5Dir_BAf.jpg&w=1080&q=75',
+            link: '',
+        }, {
+            name: 'Safa - 6ml',
+            price: 550,
+            discountedPrice: 440,
+            image: 'https://www.believerssign.com.bd/_next/image?url=https%3A%2F%2Fapi.believerssign.com.bd%2Fpublic%2Fproduct%2FAvjgdoY-Bfu5Dir_BAf.jpg&w=1080&q=75',
+            link: '',
+        }, {
+            name: 'Safa - 6ml',
+            price: 550,
+            discountedPrice: 440,
+            image: 'https://www.believerssign.com.bd/_next/image?url=https%3A%2F%2Fapi.believerssign.com.bd%2Fpublic%2Fproduct%2FAvjgdoY-Bfu5Dir_BAf.jpg&w=1080&q=75',
+            link: '',
+        }, {
+            name: 'Safa - 6ml',
+            price: 550,
+            discountedPrice: 440,
+            image: 'https://www.believerssign.com.bd/_next/image?url=https%3A%2F%2Fapi.believerssign.com.bd%2Fpublic%2Fproduct%2FAvjgdoY-Bfu5Dir_BAf.jpg&w=1080&q=75',
+            link: '',
+        }, {
+            name: 'Safa - 6ml',
+            price: 550,
+            discountedPrice: 440,
+            image: 'https://www.believerssign.com.bd/_next/image?url=https%3A%2F%2Fapi.believerssign.com.bd%2Fpublic%2Fproduct%2FAvjgdoY-Bfu5Dir_BAf.jpg&w=1080&q=75',
+            link: '',
+        }, {
+            name: 'Safa - 6ml',
+            price: 550,
+            discountedPrice: 440,
+            image: 'https://www.believerssign.com.bd/_next/image?url=https%3A%2F%2Fapi.believerssign.com.bd%2Fpublic%2Fproduct%2FAvjgdoY-Bfu5Dir_BAf.jpg&w=1080&q=75',
+            link: '',
+        }, {
+            name: 'Safa - 6ml',
+            price: 550,
+            discountedPrice: 440,
+            image: 'https://www.believerssign.com.bd/_next/image?url=https%3A%2F%2Fapi.believerssign.com.bd%2Fpublic%2Fproduct%2FAvjgdoY-Bfu5Dir_BAf.jpg&w=1080&q=75',
+            link: '',
+        }, {
+            name: 'Safa - 6ml',
+            price: 550,
+            discountedPrice: 440,
+            image: 'https://www.believerssign.com.bd/_next/image?url=https%3A%2F%2Fapi.believerssign.com.bd%2Fpublic%2Fproduct%2FAvjgdoY-Bfu5Dir_BAf.jpg&w=1080&q=75',
+            link: '',
+        }, {
+            name: 'Safa - 6ml',
+            price: 550,
+            discountedPrice: 440,
+            image: 'https://www.believerssign.com.bd/_next/image?url=https%3A%2F%2Fapi.believerssign.com.bd%2Fpublic%2Fproduct%2FAvjgdoY-Bfu5Dir_BAf.jpg&w=1080&q=75',
+            link: '',
+        }, {
+            name: 'Safa - 6ml',
+            price: 550,
+            discountedPrice: 440,
+            image: 'https://www.believerssign.com.bd/_next/image?url=https%3A%2F%2Fapi.believerssign.com.bd%2Fpublic%2Fproduct%2FAvjgdoY-Bfu5Dir_BAf.jpg&w=1080&q=75',
+            link: '',
+        }, {
+            name: 'Safa - 6ml',
+            price: 550,
+            discountedPrice: 440,
+            image: 'https://www.believerssign.com.bd/_next/image?url=https%3A%2F%2Fapi.believerssign.com.bd%2Fpublic%2Fproduct%2FAvjgdoY-Bfu5Dir_BAf.jpg&w=1080&q=75',
+            link: '',
+        }, {
+            name: 'Safa - 6ml',
+            price: 550,
+            discountedPrice: 440,
+            image: 'https://www.believerssign.com.bd/_next/image?url=https%3A%2F%2Fapi.believerssign.com.bd%2Fpublic%2Fproduct%2FAvjgdoY-Bfu5Dir_BAf.jpg&w=1080&q=75',
+            link: '',
+        }, {
+            name: 'Safa - 6ml',
+            price: 550,
+            discountedPrice: 440,
+            image: 'https://www.believerssign.com.bd/_next/image?url=https%3A%2F%2Fapi.believerssign.com.bd%2Fpublic%2Fproduct%2FAvjgdoY-Bfu5Dir_BAf.jpg&w=1080&q=75',
+            link: '',
+        }, {
+            name: 'Safa - 6ml',
+            price: 550,
+            discountedPrice: 440,
+            image: 'https://www.believerssign.com.bd/_next/image?url=https%3A%2F%2Fapi.believerssign.com.bd%2Fpublic%2Fproduct%2FAvjgdoY-Bfu5Dir_BAf.jpg&w=1080&q=75',
+            link: '',
+        }, {
+            name: 'Safa - 6ml',
+            price: 550,
+            discountedPrice: 440,
+            image: 'https://www.believerssign.com.bd/_next/image?url=https%3A%2F%2Fapi.believerssign.com.bd%2Fpublic%2Fproduct%2FAvjgdoY-Bfu5Dir_BAf.jpg&w=1080&q=75',
+            link: '',
+        }, {
+            name: 'Safa - 6ml',
+            price: 550,
+            discountedPrice: 440,
+            image: 'https://www.believerssign.com.bd/_next/image?url=https%3A%2F%2Fapi.believerssign.com.bd%2Fpublic%2Fproduct%2FAvjgdoY-Bfu5Dir_BAf.jpg&w=1080&q=75',
+            link: '',
+        }, {
+            name: 'Safa - 6ml',
+            price: 550,
+            discountedPrice: 440,
+            image: 'https://www.believerssign.com.bd/_next/image?url=https%3A%2F%2Fapi.believerssign.com.bd%2Fpublic%2Fproduct%2FAvjgdoY-Bfu5Dir_BAf.jpg&w=1080&q=75',
+            link: '',
+        }, {
+            name: 'Safa - 6ml',
+            price: 550,
+            discountedPrice: 440,
+            image: 'https://www.believerssign.com.bd/_next/image?url=https%3A%2F%2Fapi.believerssign.com.bd%2Fpublic%2Fproduct%2FAvjgdoY-Bfu5Dir_BAf.jpg&w=1080&q=75',
+            link: '',
+        }, {
+            name: 'Safa - 6ml',
+            price: 550,
+            discountedPrice: 440,
+            image: 'https://www.believerssign.com.bd/_next/image?url=https%3A%2F%2Fapi.believerssign.com.bd%2Fpublic%2Fproduct%2FAvjgdoY-Bfu5Dir_BAf.jpg&w=1080&q=75',
+            link: '',
+        }, {
+            name: 'Safa - 6ml',
+            price: 550,
+            discountedPrice: 440,
+            image: 'https://www.believerssign.com.bd/_next/image?url=https%3A%2F%2Fapi.believerssign.com.bd%2Fpublic%2Fproduct%2FAvjgdoY-Bfu5Dir_BAf.jpg&w=1080&q=75',
+            link: '',
+        }, {
+            name: 'Safa - 6ml',
+            price: 550,
+            discountedPrice: 440,
+            image: 'https://www.believerssign.com.bd/_next/image?url=https%3A%2F%2Fapi.believerssign.com.bd%2Fpublic%2Fproduct%2FAvjgdoY-Bfu5Dir_BAf.jpg&w=1080&q=75',
+            link: '',
+        }, {
+            name: 'Safa - 6ml',
+            price: 550,
+            discountedPrice: 440,
+            image: 'https://www.believerssign.com.bd/_next/image?url=https%3A%2F%2Fapi.believerssign.com.bd%2Fpublic%2Fproduct%2FAvjgdoY-Bfu5Dir_BAf.jpg&w=1080&q=75',
+            link: '',
+        },
+    ]
     return (
-        <div>seriouspremium</div>
+        <div>
+            <div className="px-4 py-4 grid grid-cols-2 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
+                {seriouspremium.map((product, index) => (
+                    <ProductCard key={index} {...product} />
+                ))}
+            </div>
+        </div>
     )
 }
 
--- src/panjabi/elegant.jsx
+++ src/panjabi/elegant.jsx
@@ -1,8 +1,241 @@
 import React from 'react'
+import ProductCard from '../componentes/Productcard'
 
 const elegant = () => {
+    const elegant = [
+        {
+            name: 'Royal Sapphire - 6ml',
+            price: 550,
+            discountedPrice: 440,
+            image: 'https://www.believerssign.com.bd/_next/image?url=https%3A%2F%2Fapi.believerssign.com.bd%2Fpublic%2Fproduct%2F_2OLzuLO7BvgTUbFgLR.jpg&w=1080&q=75',
+            link: '',
+        },
+        {
+            name: 'Red African - 6ml',
+            price: 550,
+            discountedPrice: 440,
+            image: 'https://www.believerssign.com.bd/_next/image?url=https%3A%2F%2Fapi.believerssign.com.bd%2Fpublic%2Fproduct%2F_2OLzuLO7BvgTUbFgLR.jpg&w=1080&q=75',
+            link: '',
+        }, {
+            name: 'Red African - 6ml',
+            price: 550,
+            discountedPrice: 440,
+            image: 'https://www.believerssign.com.bd/_next/image?url=https%3A%2F%2Fapi.believerssign.com.bd%2Fpublic%2Fproduct%2F_2OLzuLO7BvgTUbFgLR.jpg&w=1080&q=75',
+            link: '',
+        }, {
+            name: 'Red African - 6ml',
+            price: 550,
+            discountedPrice: 440,
+            image: 'https://www.believerssign.com.bd/_next/image?url=https%3A%2F%2Fapi.believerssign.com.bd%2Fpublic%2Fproduct%2F_2OLzuLO7BvgTUbFgLR.jpg&w=1080&q=75',
+            link: '',
+        }, {
+            name: 'Red African - 6ml',
+            price: 550,
+            discountedPrice: 440,
+            image: 'https://www.believerssign.com.bd/_next/image?url=https%3A%2F%2Fapi.believerssign.com.bd%2Fpublic%2Fproduct%2F_2OLzuLO7BvgTUbFgLR.jpg&w=1080&q=75',
+            link: '',
+        }, {
+            name: 'Red African - 6ml',
+            price: 550,
+            discountedPrice: 440,
+            image: 'https://www.believerssign.com.bd/_next/image?url=https%3A%2F%2Fapi.believerssign.com.bd%2Fpublic%2Fproduct%2F_2OLzuLO7BvgTUbFgLR.jpg&w=1080&q=75',
+            link: '',
+        }, {
+            name: 'Red African - 6ml',
+            price: 550,
+            discountedPrice: 440,
+            image: 'https://www.believerssign.com.bd/_next/image?url=https%3A%2F%2Fapi.believerssign.com.bd%2Fpublic%2Fproduct%2FT2uC7zyeb6J9nr5YMTz.jpg&w=1080&q=75',
+            link: '',
+        }, {
+            name: 'Red African - 6ml',
+            price: 550,
+            discountedPrice: 440,
+            image: 'https://www.believerssign.com.bd/_next/image?url=https%3A%2F%2Fapi.believerssign.com.bd%2Fpublic%2Fproduct%2FT2uC7zyeb6J9nr5YMTz.jpg&w=1080&q=75',
+            link: '',
+        }, {
+            name: 'Red African - 6ml',
+            price: 550,
+            discountedPrice: 440,
+            image: 'https://www.believerssign.com.bd/_next/image?url=https%3A%2F%2Fapi.believerssign.com.bd%2Fpublic%2Fproduct%2FT2uC7zyeb6J9nr5YMTz.jpg&w=1080&q=75',
+            link: '',
+        }, {
+            name: 'Red African - 6ml',
+            price: 550,
+            discountedPrice: 440,
+            image: 'https://www.believerssign.com.bd/_next/image?url=https%3A%2F%2Fapi.believerssign.com.bd%2Fpublic%2Fproduct%2FT2uC7zyeb6J9nr5YMTz.jpg&w=1080&q=75',
+            link: '',
+        }, {
+            name: 'Red African - 6ml',
+            price: 550,
+            discountedPrice: 440,
+            image: 'https://www.believerssign.com.bd/_next/image?url=https%3A%2F%2Fapi.believerssign.com.bd%2Fpublic%2Fproduct%2FT2uC7zyeb6J9nr5YMTz.jpg&w=1080&q=75',
+            link: '',
+        }, {
+            name: 'Red African - 6ml',
+            price: 550,
+            discountedPrice: 440,
+            image: 'https://www.believerssign.com.bd/_next/image?url=https%3A%2F%2Fapi.believerssign.com.bd%2Fpublic%2Fproduct%2FT2uC7zyeb6J9nr5YMTz.jpg&w=1080&q=75',
+            link: '',
+        }, {
+            name: 'Red African - 6ml',
+            price: 550,
+            discountedPrice: 440,
+            image: 'https://www.believerssign.com.bd/_next/image?url=https%3A%2F%2Fapi.believerssign.com.bd%2Fpublic%2Fproduct%2FT2uC7zyeb6J9nr5YMTz.jpg&w=1080&q=75',
+            link: '',
+        }, {
+            name: 'Red African - 6ml',
+            price: 550,
+            discountedPrice: 440,
+            image: 'https://www.believerssign.com.bd/_next/image?url=https%3A%2F%2Fapi.believerssign.com.bd%2Fpublic%2Fproduct%2FT2uC7zyeb6J9nr5YMTz.jpg&w=1080&q=75',
+            link: '',
+        }, {
+            name: 'Red African - 6ml',
+            price: 550,
+            discountedPrice: 440,
+            image: 'https://www.believerssign.com.bd/_next/image?url=https%3A%2F%2Fapi.believerssign.com.bd%2Fpublic%2Fproduct%2FT2uC7zyeb6J9nr5YMTz.jpg&w=1080&q=75',
+            link: '',
+        }, {
+            name: 'Red African - 6ml',
+            price: 550,
+            discountedPrice: 440,
+            image: 'https://www.believerssign.com.bd/_next/image?url=https%3A%2F%2Fapi.believerssign.com.bd%2Fpublic%2Fproduct%2FT2uC7zyeb6J9nr5YMTz.jpg&w=1080&q=75',
+            link: '',
+        }, {
+            name: 'Red African - 6ml',
+            price: 550,
+            discountedPrice: 440,
+            image: 'https://www.believerssign.com.bd/_next/image?url=https%3A%2F%2Fapi.believerssign.com.bd%2Fpublic%2Fproduct%2FT2uC7zyeb6J9nr5YMTz.jpg&w=1080&q=75',
+            link: '',
+        }, {
+            name: 'Red African - 6ml',
+            price: 550,
+            discountedPrice: 440,
+            image: 'https://www.believerssign.com.bd/_next/image?url=https%3A%2F%2Fapi.believerssign.com.bd%2Fpublic%2Fproduct%2FT2uC7zyeb6J9nr5YMTz.jpg&w=1080&q=75',
+            link: '',
+        }, {
+            name: 'Red African - 6ml',
+            price: 550,
+            discountedPrice: 440,
+            image: 'https://www.believerssign.com.bd/_next/image?url=https%3A%2F%2Fapi.believerssign.com.bd%2Fpublic%2Fproduct%2FT2uC7zyeb6J9nr5YMTz.jpg&w=1080&q=75',
+            link: '',
+        }, {
+            name: 'Red African - 6ml',
+            price: 550,
+            discountedPrice: 440,
+            image: 'https://www.believerssign.com.bd/_next/image?url=https%3A%2F%2Fapi.believerssign.com.bd%2Fpublic%2Fproduct%2FT2uC7zyeb6J9nr5YMTz.jpg&w=1080&q=75',
+            link: '',
+        }, {
+            name: 'Red African - 6ml',
+            price: 550,
+            discountedPrice: 440,
+            image: 'https://www.believerssign.com.bd/_next/image?url=https%3A%2F%2Fapi.believerssign.com.bd%2Fpublic%2Fproduct%2FT2uC7zyeb6J9nr5YMTz.jpg&w=1080&q=75',
+            link: '',
+        }, {
+            name: 'Red African - 6ml',
+            price: 550,
+            discountedPrice: 440,
+            image: 'https://www.believerssign.com.bd/_next/image?url=https%3A%2F%2Fapi.believerssign.com.bd%2Fpublic%2Fproduct%2FT2uC7zyeb6J9nr5YMTz.jpg&w=1080&q=75',
+            link: '',
+        }, {
+            name: 'Red African - 6ml',
+            price: 550,
+            discountedPrice: 440,
+            image: 'https://www.believerssign.com.bd/_next/image?url=https%3A%2F%2Fapi.believerssign.com.bd%2Fpublic%2Fproduct%2FT2uC7zyeb6J9nr5YMTz.jpg&w=1080&q=75',
+            link: '',
+        }, {
+            name: 'Red African - 6ml',
+            price: 550,
+            discountedPrice: 440,
+            image: 'https://www.believerssign.com.bd/_next/image?url=https%3A%2F%2Fapi.believerssign.com.bd%2Fpublic%2Fproduct%2FT2uC7zyeb6J9nr5YMTz.jpg&w=1080&q=75',
+            link: '',
+        }, {
+            name: 'Red African - 6ml',
+            price: 550,
+            discountedPrice: 440,
+            image: 'https://www.believerssign.com.bd/_next/image?url=https%3A%2F%2Fapi.believerssign.com.bd%2Fpublic%2Fproduct%2FT2uC7zyeb6J9nr5YMTz.jpg&w=1080&q=75',
+            link: '',
+        }, {
+            name: 'Red African - 6ml',
+            price: 550,
+            discountedPrice: 440,
+            image: 'https://www.believerssign.com.bd/_next/image?url=https%3A%2F%2Fapi.believerssign.com.bd%2Fpublic%2Fproduct%2FT2uC7zyeb6J9nr5YMTz.jpg&w=1080&q=75',
+            link: '',
+        }, {
+            name: 'Red African - 6ml',
+            price: 550,
+            discountedPrice: 440,
+            image: 'https://www.believerssign.com.bd/_next/image?url=https%3A%2F%2Fapi.believerssign.com.bd%2Fpublic%2Fproduct%2FT2uC7zyeb6J9nr5YMTz.jpg&w=1080&q=75',
+            link: '',
+        }, {
+            name: 'Red African - 6ml',
+            price: 550,
+            discountedPrice: 440,
+            image: 'https://www.believerssign.com.bd/_next/image?url=https%3A%2F%2Fapi.believerssign.com.bd%2Fpublic%2Fproduct%2FT2uC7zyeb6J9nr5YMTz.jpg&w=1080&q=75',
+            link: '',
+        }, {
+            name: 'Red African - 6ml',
+            price: 550,
+            discountedPrice: 440,
+            image: 'https://www.believerssign.com.bd/_next/image?url=https%3A%2F%2Fapi.believerssign.com.bd%2Fpublic%2Fproduct%2FT2uC7zyeb6J9nr5YMTz.jpg&w=1080&q=75',
+            link: '',
+        }, {
+            name: 'Red African - 6ml',
+            price: 550,
+            discountedPrice: 440,
+            image: 'https://www.believerssign.com.bd/_next/image?url=https%3A%2F%2Fapi.believerssign.com.bd%2Fpublic%2Fproduct%2FT2uC7zyeb6J9nr5YMTz.jpg&w=1080&q=75',
+            link: '',
+        }, {
+            name: 'Red African - 6ml',
+            price: 550,
+            discountedPrice: 440,
+            image: 'https://www.believerssign.com.bd/_next/image?url=https%3A%2F%2Fapi.believerssign.com.bd%2Fpublic%2Fproduct%2FT2uC7zyeb6J9nr5YMTz.jpg&w=1080&q=75',
+            link: '',
+        }, {
+            name: 'Red African - 6ml',
+            price: 550,
+            discountedPrice: 440,
+            image: 'https://www.believerssign.com.bd/_next/image?url=https%3A%2F%2Fapi.believerssign.com.bd%2Fpublic%2Fproduct%2FT2uC7zyeb6J9nr5YMTz.jpg&w=1080&q=75',
+            link: '',
+        }, {
+            name: 'Red African - 6ml',
+            price: 550,
+            discountedPrice: 440,
+            image: 'https://www.believerssign.com.bd/_next/image?url=https%3A%2F%2Fapi.believerssign.com.bd%2Fpublic%2Fproduct%2FT2uC7zyeb6J9nr5YMTz.jpg&w=1080&q=75',
+            link: '',
+        }, {
+            name: 'Red African - 6ml',
+            price: 550,
+            discountedPrice: 440,
+            image: 'https://www.believerssign.com.bd/_next/image?url=https%3A%2F%2Fapi.believerssign.com.bd%2Fpublic%2Fproduct%2FT2uC7zyeb6J9nr5YMTz.jpg&w=1080&q=75',
+            link: '',
+        }, {
+            name: 'Red African - 6ml',
+            price: 550,
+            discountedPrice: 440,
+            image: 'https://www.believerssign.com.bd/_next/image?url=https%3A%2F%2Fapi.believerssign.com.bd%2Fpublic%2Fproduct%2FT2uC7zyeb6J9nr5YMTz.jpg&w=1080&q=75',
+            link: '',
+        }, {
+            name: 'Red African - 6ml',
+            price: 550,
+            discountedPrice: 440,
+            image: 'https://www.believerssign.com.bd/_next/image?url=https%3A%2F%2Fapi.believerssign.com.bd%2Fpublic%2Fproduct%2FT2uC7zyeb6J9nr5YMTz.jpg&w=1080&q=75',
+            link: '',
+        }, {
+            name: 'Red African - 6ml',
+            price: 550,
+            discountedPrice: 440,
+            image: 'https://www.believerssign.com.bd/_next/image?url=https%3A%2F%2Fapi.believerssign.com.bd%2Fpublic%2Fproduct%2FT2uC7zyeb6J9nr5YMTz.jpg&w=1080&q=75',
+            link: '',
+        },
+    ]
     return (
-        <div>elegant</div>
+        <div>
+            <div className="px-4 py-4 grid grid-cols-2 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
+                {elegant.map((product, index) => (
+                    <ProductCard key={index} {...product} />
+                ))}
+            </div>
+        </div>
     )
 }
 
