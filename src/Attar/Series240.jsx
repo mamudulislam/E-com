3163d1c70871651cd72346cba15a5d60421b6972
Mamudul_islam <mamuduli153@gmail.com>
2025-04-13 20:39:00 +0600

    add data

--- src/Attar/Series240.jsx
+++ src/Attar/Series240.jsx
@@ -1,8 +1,128 @@
 import React from 'react'
+import ProductCard from '../componentes/Productcard'
 
 const Series240 = () => {
+    const Series240 = [
+        {
+            name: 'Royal Sapphire - 6ml',
+            price: 550,
+            discountedPrice: 440,
+            image: 'https://www.believerssign.com.bd/_next/image?url=https%3A%2F%2Fapi.believerssign.com.bd%2Fpublic%2Fproduct%2Fvp28iiz48ip7WL-WYHr.jpg&w=1080&q=75',
+            link: '',
+        },
+        {
+            name: 'Red African - 6ml',
+            price: 550,
+            discountedPrice: 440,
+            image: 'https://www.believerssign.com.bd/_next/image?url=https%3A%2F%2Fapi.believerssign.com.bd%2Fpublic%2Fproduct%2Fvp28iiz48ip7WL-WYHr.jpg&w=1080&q=75',
+            link: '',
+        }, {
+            name: 'Safa - 6ml',
+            price: 550,
+            discountedPrice: 440,
+            image: 'https://www.believerssign.com.bd/_next/image?url=https%3A%2F%2Fapi.believerssign.com.bd%2Fpublic%2Fproduct%2Fvp28iiz48ip7WL-WYHr.jpg&w=1080&q=75',
+            link: '',
+        }, {
+            name: 'Safa - 6ml',
+            price: 550,
+            discountedPrice: 440,
+            image: 'https://www.believerssign.com.bd/_next/image?url=https%3A%2F%2Fapi.believerssign.com.bd%2Fpublic%2Fproduct%2Fvp28iiz48ip7WL-WYHr.jpg&w=1080&q=75',
+            link: '',
+        }, {
+            name: 'Safa - 6ml',
+            price: 550,
+            discountedPrice: 440,
+            image: 'https://www.believerssign.com.bd/_next/image?url=https%3A%2F%2Fapi.believerssign.com.bd%2Fpublic%2Fproduct%2Fvp28iiz48ip7WL-WYHr.jpg&w=1080&q=75',
+            link: '',
+        }, {
+            name: 'Safa - 6ml',
+            price: 550,
+            discountedPrice: 440,
+            image: 'https://www.believerssign.com.bd/_next/image?url=https%3A%2F%2Fapi.believerssign.com.bd%2Fpublic%2Fproduct%2Fvp28iiz48ip7WL-WYHr.jpg&w=1080&q=75',
+            link: '',
+        }, {
+            name: 'Safa - 6ml',
+            price: 550,
+            discountedPrice: 440,
+            image: 'https://www.believerssign.com.bd/_next/image?url=https%3A%2F%2Fapi.believerssign.com.bd%2Fpublic%2Fproduct%2Fvp28iiz48ip7WL-WYHr.jpg&w=1080&q=75',
+            link: '',
+        }, {
+            name: 'Safa - 6ml',
+            price: 550,
+            discountedPrice: 440,
+            image: 'https://www.believerssign.com.bd/_next/image?url=https%3A%2F%2Fapi.believerssign.com.bd%2Fpublic%2Fproduct%2Fvp28iiz48ip7WL-WYHr.jpg&w=1080&q=75',
+            link: '',
+        }, {
+            name: 'Safa - 6ml',
+            price: 550,
+            discountedPrice: 440,
+            image: 'https://www.believerssign.com.bd/_next/image?url=https%3A%2F%2Fapi.believerssign.com.bd%2Fpublic%2Fproduct%2Fvp28iiz48ip7WL-WYHr.jpg&w=1080&q=75',
+            link: '',
+        }, {
+            name: 'Safa - 6ml',
+            price: 550,
+            discountedPrice: 440,
+            image: 'https://www.believerssign.com.bd/_next/image?url=https%3A%2F%2Fapi.believerssign.com.bd%2Fpublic%2Fproduct%2Fvp28iiz48ip7WL-WYHr.jpg&w=1080&q=75',
+            link: '',
+        }, {
+            name: 'Safa - 6ml',
+            price: 550,
+            discountedPrice: 440,
+            image: 'https://www.believerssign.com.bd/_next/image?url=https%3A%2F%2Fapi.believerssign.com.bd%2Fpublic%2Fproduct%2Fvp28iiz48ip7WL-WYHr.jpg&w=1080&q=75',
+            link: '',
+        }, {
+            name: 'Safa - 6ml',
+            price: 550,
+            discountedPrice: 440,
+            image: 'https://www.believerssign.com.bd/_next/image?url=https%3A%2F%2Fapi.believerssign.com.bd%2Fpublic%2Fproduct%2Fvp28iiz48ip7WL-WYHr.jpg&w=1080&q=75',
+            link: '',
+        }, {
+            name: 'Safa - 6ml',
+            price: 550,
+            discountedPrice: 440,
+            image: 'https://www.believerssign.com.bd/_next/image?url=https%3A%2F%2Fapi.believerssign.com.bd%2Fpublic%2Fproduct%2Fvp28iiz48ip7WL-WYHr.jpg&w=1080&q=75',
+            link: '',
+        }, {
+            name: 'Safa - 6ml',
+            price: 550,
+            discountedPrice: 440,
+            image: 'https://www.believerssign.com.bd/_next/image?url=https%3A%2F%2Fapi.believerssign.com.bd%2Fpublic%2Fproduct%2Fvp28iiz48ip7WL-WYHr.jpg&w=1080&q=75',
+            link: '',
+        }, {
+            name: 'Safa - 6ml',
+            price: 550,
+            discountedPrice: 440,
+            image: 'https://www.believerssign.com.bd/_next/image?url=https%3A%2F%2Fapi.believerssign.com.bd%2Fpublic%2Fproduct%2Fvp28iiz48ip7WL-WYHr.jpg&w=1080&q=75',
+            link: '',
+        }, {
+            name: 'Safa - 6ml',
+            price: 550,
+            discountedPrice: 440,
+            image: 'https://www.believerssign.com.bd/_next/image?url=https%3A%2F%2Fapi.believerssign.com.bd%2Fpublic%2Fproduct%2Fvp28iiz48ip7WL-WYHr.jpg&w=1080&q=75',
+            link: '',
+        }, {
+            name: 'Safa - 6ml',
+            price: 550,
+            discountedPrice: 440,
+            image: 'https://www.believerssign.com.bd/_next/image?url=https%3A%2F%2Fapi.believerssign.com.bd%2Fpublic%2Fproduct%2Fvp28iiz48ip7WL-WYHr.jpg&w=1080&q=75',
+            link: '',
+        }, {
+            name: 'Safa - 6ml',
+            price: 550,
+            discountedPrice: 440,
+            image: 'https://www.believerssign.com.bd/_next/image?url=https%3A%2F%2Fapi.believerssign.com.bd%2Fpublic%2Fproduct%2Fvp28iiz48ip7WL-WYHr.jpg&w=1080&q=75',
+            link: '',
+        },
+    ];
+
     return (
-        <div>Series240</div>
+        <div>
+            <div className="px-4 py-4 grid grid-cols-2 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
+                {Series240.map((product, index) => (
+                    <ProductCard key={index} {...product} />
+                ))}
+            </div>
+        </div>
     )
 }
 
