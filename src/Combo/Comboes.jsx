f7668a74d8bb24359b294dc94c41bbc3435b734f
Mamudul_islam <mamuduli153@gmail.com>
2025-04-15 23:42:17 +0600

    combo

--- src/Combo/Comboes.jsx
+++ src/Combo/Comboes.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Combo = () => {
     return (
         <div>
-            <h1 className='p-2.5 text-3xl text-gray-500 my-6 mx-25'>
+            <h1 className='p-2.5 text-3xl text-gray-500 my-6 mx-27'>
                 THis is combo product
             </h1>
         </div>
