3830fd1f3fb8b7541297d5358944bd11d5853e33
Mamudul_islam <mamuduli153@gmail.com>
2025-04-16 21:33:42 +0600

    commit

--- src/Combo/Comboes.jsx
+++ src/Combo/Comboes.jsx
@@ -5,7 +5,6 @@ const Combo = () => {
         <div>
             <h1 className='p-2.5 text-3xl text-gray-500 my-6 mx-27'>
                 THis is combo product
-                <p>hi iam commit</p>
             </h1>
         </div>
     )
